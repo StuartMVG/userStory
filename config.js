b487e2d6ba57cb8f6ca8adbec96f506083adf214
Stuart Gardner <stuartmvg@gmail.com>
2016-04-04 17:24:36 -0400

    Same place

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports ={
 
-  "database": "",
+  "database": "mongodb://root:abc123@ds015690.mlab.com:15690/userstorysmvg",
   "port": process.env.PORT || 3000,
   "secretKey": "YourSecretKey"
 }
